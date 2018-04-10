179e577d1f42853e2d4b68057f6b09457110740d
Dylan D'Costa <ddcosta595@gmail.com>
2018-04-09 22:23:37 -0600

    final

--- src/containers/stocks/StocksBrowser.js
+++ src/containers/stocks/StocksBrowser.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import stocks from '../../data/stocks.json';
 import { NavLink } from 'react-router-dom';
 import StockListItem from './StockListItem'
+import axios from 'axios';
+
 
 class StocksBrowser extends Component {
     constructor(props) {
         super(props);
-        this.state = {currentStock: 1, stockList: stocks};
+        this.state = {currentComp: 1, compList: stocks};
+    }
+    
+    componentDidMount()
+    {
+        axios.get('https://floating-gorge-bend.herokuapp.com/api/companies').then(
+            response => { this.setState({compList : response.data}); 
+                        }).catch(function(error) { alert('Error with API call...error=' + error); });
     }
     
     render() {
@@ -17,24 +25,24 @@ class StocksBrowser extends Component {
                         <nav className="breadcrumb navbar-item" aria-label="breadcrumbs">
                             <ul>
                                 <li><NavLink to={ {pathname: "/home"} }><span>Home</span></NavLink></li>
-                                <li className='is-active'><a aria-current="page"><span>Stocks</span></a></li>
+                                <li className='is-active'><a aria-current="page"><span>Companies</span></a></li>
                             </ul>
                         </nav>
                     </div>
                     <div className="container">
                         <div className="navbar">
-                            <h1 className="navbar-item title is-5">Stocks</h1>
+                            <h1 className="navbar-item title is-5">Companies</h1>
                         </div>
                         <article>
                             <nav className="panel">
                             {
-                                stocks.map((stock, index) => {
+                                compList.map((comp, index) => {
                                     let activeClass = "";
                                     return(
                                         <StockListItem
                                                        index={index}
-                                                       identifier={stock.symbol}
-                                                       symbol={stock.symbol} active={activeClass}>{stock.name}</StockListItem>
+                                                       identifier={comp.symbol}
+                                                       symbol={comp.symbol} active={activeClass}>{comp.name}</StockListItem>
                                     );
                                 }
                                 )
